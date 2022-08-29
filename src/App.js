7ef93695973de4700d1da07fe5f3fe7f048e3ea9
Edward Ho <pythoned77@gmail.com>
2022-08-29 11:33:07 +0800

    added Axios and created get API to searchSneaker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [sneakerName, setSneakerName] = useState("");
+
+  const searchSneaker = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${sneakerName}`).then(
+      (response) => {
+        console.log(response);
+      }
+    );
+  };
+
   return (
     <div className="App">
       <div className="TitleSection">
         <h1>Sneaker Information</h1>
-        <input type="text" />
-        <button>Search Sneaker</button>
+        <input
+          type="text"
+          onChange={(e) => {
+            setSneakerName(e.target.value);
+          }}
+        />
+        <button onClick={searchSneaker}>Search Sneaker</button>
       </div>
     </div>
   );
